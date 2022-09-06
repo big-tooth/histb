faa213fab2766a42e9a3bb2368db26c6ffd4d0f1
teasiu <teasiu@qq.com>
2022-09-07 00:32:54 +0800

    fix erro noip ddns script

--- package_files/others/pre_files/common/home/ubuntu/client-mode/ddns_noip.sh
+++ package_files/others/pre_files/common/home/ubuntu/client-mode/ddns_noip.sh
@@ -9,7 +9,7 @@ curExIP=`curl http://ip.3322.net | tee ddns_cur_ip2.dat`
 
 if [ "$curExIP" != "$oldIP" ];then
     echo "Refresh IP from $oldIP to: $curExIP"
-    noipcom="http://$user:$password@dynupdate.no-ip.com/nic/update?hostname=$hostaname&myip=$curExIP"
+    noipcom="http://$user:$password@dynupdate.no-ip.com/nic/update?hostname=$hostname&myip=$curExIP"
     curl $noipcom
 else
     echo "No change on external IP - $curExIP"
