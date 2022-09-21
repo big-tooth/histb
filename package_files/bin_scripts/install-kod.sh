9c93af001ff032f5d7e7569417871b778d418141
teasiu <teasiu@qq.com>
2022-09-21 20:46:42 +0800

    fix kod script

--- package_files/bin_scripts/install-kod.sh
+++ package_files/bin_scripts/install-kod.sh
@@ -9,6 +9,12 @@ if [ ! -f "/etc/debian_version" ]; then
     exit 1
 fi
 local_ip=$(ifconfig eth0 | grep '\<inet\>'| grep -v '127.0.0.1' | awk '{ print $2}' | awk 'NR==1')
+if [ -d /opt/kod ]; then
+  rm -rf /opt/kod
+fi
+if [ -h /var/www/html/kod ]; then
+  rm /var/www/html/kod
+fi
 mkdir -p /opt/kod
 cd /opt/kod
 wget https://static.kodcloud.com/update/download/kodbox.1.34.zip
